625a482a01cb0417ecaba25dc1a2b30f270f2dc9
William Broniec <williambroniec@gmail.com>
2018-04-07 16:51:55 -0400

    fixed some cli things

--- cli.js
+++ cli.js
@@ -5,30 +5,33 @@ const bulb = ['192.168.1.6']
 const light = new TPLSmartDevice(bulb[0])
 const mode = process.argv[2]
 
-function change_color(color, transition=0) {
-	var colors = convert.hex.hsl(color)
+function change_color(hue, sat) {
 	var opt = {}
 
-	opt.hue = colors[0]
-	opt.saturation = colors[1]
+	opt.hue = hue
+	opt.saturation = sat
 	opt.brightness = 100
-	light.power(true,transition,opt).catch(err => console.error(err))             
+	light.power(true, 0, opt).catch(err => console.error(err))             
 }
 
-function off() {
-	light.power(false,0).catch(err => console.error(err))
+function set(setting) {
+	light.power(setting, 0).catch(err => console.error(err))
 }
 
-if (mode == 'on' || mode == 'white') {
-	change_color('ffffff')
+if (mode == 'on') {
+	set(true)
 } else if (mode == 'off') {
-	off()
-} else if (mode == 'red' || mode == 'sexy') {
-	change_color('ff0000')
-} else if (mode == 'green' || mode == '420') {
-	change_color('00ff00')
+	set(false)
+} else if (mode == 'white' || mode == 'warm') {
+	change_color(0, 0)
+} else if (mode == 'cool') {
+	change_color(240, 50)
+} else if (mode == 'red') {
+	change_color(0, 100)
+} else if (mode == 'green') {
+	change_color(120, 100)
 } else if (mode == 'blue') {
-	change_color('0000ff')
+	change_color(240, 100)
 } else if (mode == 'purple') {
-	change_color('a020f0')
+	change_color(300, 100)
 }
