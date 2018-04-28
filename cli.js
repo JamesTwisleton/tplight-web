cfc5f1cd16e45ed5a4212925c5511481acbd1159
William Broniec <williambroniec@gmail.com>
2018-04-28 11:59:46 -0400

    dark theme + more arduino stuff

--- cli.js
+++ cli.js
@@ -5,6 +5,8 @@ const sleep = require('system-sleep')
 const bulb = ['192.168.1.6']
 const light = new TPLSmartDevice(bulb[0])
 const mode = process.argv[2]
+const arduino = (process.argv.length == 4 && process.argv[3] == '-r')
+console.log(arduino)
 const transition = 10200
 
 function change_color(hue, sat, trans=0) {
@@ -14,23 +16,18 @@ function change_color(hue, sat, trans=0) {
 	opt.saturation = sat
 	opt.brightness = 100
 	light.power(true, trans, opt).catch(err => console.error(err))             
+	console.log('t2')
 }
 
-function set(setting) {
+function toggle(setting) {
 	light.power(setting, 0).catch(err => console.error(err))
 }
 
-var SerialPort = require('serialport')
-
-var port = new SerialPort('COM3', {
-	baudRate: 9600
-})
-
 function parse() {
 	if (mode == 'on') {
-		set(true)
+		toggle(true)
 	} else if (mode == 'off') {
-		set(false)
+		toggle(false)
 	} else if (mode == 'white' || mode == 'warm') {
 		change_color(0, 0)
 	} else if (mode == 'cool') {
@@ -44,7 +41,6 @@ function parse() {
 	} else if (mode == 'purple') {
 		change_color(300, 100)
 	} else if (mode == 'relax') {
-		port.write('\x31')
 		change_color(240, 100)
 		while (true) {
 			change_color(0, 100, transition)
@@ -55,13 +51,26 @@ function parse() {
 	}
 }
 
-port.on("open", function () {
-	console.log("open")
+if (arduino) {
+	var SerialPort = require('serialport')
+	var port = new SerialPort('COM3', {
+		baudRate: 9600
+	})
 
-	port.on("data", function(data) {
-		console.log("data received: " + data);
+	port.on('open', function () {
+		console.log('open')
+
+		port.on('data', function(data) {
+			console.log('data received: ' + data);
+		});
+
+		// TODO: find lower bound of this delay
+		sleep(5000)
+		port.write('\x31')
+		parse()
 	});
-
-	sleep(5000)
+} else {
 	parse()
-});
+	console.log('t3')
+}
+console.log('t4')
