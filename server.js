114ab5815078263eb7a3e0cd8f58d4b3c4c3f6fc
JamesTwisleton <ajtwisleton@gmail.com>
2019-01-10 21:15:11 +0000

    refactored server

--- server.js
+++ server.js
@@ -23,12 +23,15 @@ var fadeOn = false;
 var currentPattern = 0;
 var bpm = 60;
 var transition = 60000 / bpm;
-var fade = Math.round(transition / 3);
+var fadeRatio = config.fadeRatio;
+var fade = Math.round(transition / fadeRatio);
+var bpmMin = config.bpmMin;
+var bpmMax = config.bpmMax;
 
 /**
- * Express server setup.
+ * Express server and socket setup.
  */
-server.listen(80);
+server.listen(port_no);
 app.use(express.static(`${__dirname}/public`));
 io.on('connection', function (socket) {
     socket.emit('init', {
@@ -39,163 +42,68 @@ io.on('connection', function (socket) {
         patterns: patterns
     });
     socket.on('speedup', function (data) {
-        console.log("speedup");
+        speed_up();
     });
-    socket.on('cyclingtoggled', function (data) {
-        console.log("cyclingtoggled");
+    socket.on('slowdown', function (data) {
+        slow_down();
     });
-    socket.on('fadetoggled', function (data) {
-        console.log("fadetoggled");
+    socket.on('cyclingon', function (data) {
+        cyclingOn = true;
+        cycle();
+    });
+    socket.on('cyclingoff', function (data) {
+        cyclingOn = false;
+    });
+    socket.on('fadeon', function (data) {
+        fade_on();
+    });
+    socket.on('fadeoff', function (data) {
+        fade_off();
     });
     socket.on('changepattern', function (data) {
         console.log(data);
     });
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/speedup', function (req, res) {
-    speed_up();
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-
-});
-app.get('/slowdown', function (req, res) {
-    slow_down();
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-
-});
-app.get('/transition', function (req, res) {
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-});
-app.get('/bpm', function (req, res) {
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-});
-app.get('/color', function (req, res) {
-    res.set("Connection", "close");
-    res.send(globalColor);
-});
-app.get('/changetransition', function (req, res) {
-
-    transition = parseInt(req.query.transition)
-});
-app.get('/changecolor', function (req, res) {
-    console.log("color change")
-    change_color(req.query.color);
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-});
-app.get('/start', function (req, res) {
-    console.log("start invoked")
-    cyclingOn = true
-    cycle_colors()
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-});
-app.get('/stop', function (req, res) {
-    console.log("stop invoked")
-    cyclingOn = false
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-});
-app.get('/mode', function (req, res) {
-    next_mode()
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-});
-app.get('/fadeon', function (req, res) {
-    fade_on();
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-});
-app.get('/fadeoff', function (req, res) {
-    fade_off();
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-});
-app.get('/cycling', function (req, res) {
-    res.set("Connection", "close");
-    res.send(cyclingOn);
-});
-app.get('/fading', function (req, res) {
-    res.set("Connection", "close");
-    res.send(fadeOn);
-});
-app.get('/changepattern', function (req, res) {
-    patternMode = parseInt(req.query.pattern);
-    console.log("pattern change : " + req.query.pattern);
-    res.set("Connection", "close");
-    res.send(bpm.toString());
-});
-app.get('/colors', function (req, res) {
-    var patternRequested = parseInt(req.query.pattern);
-    console.log("pattern requested: " + patternRequested);
-    res.set("Connection", "close");
-
-    switch (patternMode) {
-        case 0:
-            res.send(colors0);
-            break
-        case 1:
-            res.send(colors1);
-            break
-        case 2:
-            res.send(colors2);
-            break
-        case 3:
-            res.send(colors3);
-            break
-        case 4:
-            res.send(colors4);
-            break
-        case 5:
-            res.send(colors5);
-            break
-
-    }
 });
 
 function speed_up() {
-    if (bpm < 200) {
-        bpm = bpm + 1;
+    if (bpm < bpmMax) {
+        bpm = bpm + 5;
         transition = 60000 / bpm;
-        fade = Math.round(transition / 3);
-        console.log("speed up");
+        fade = Math.round(transition / fadeRatio);
+        console.log("Speeding up to " + bpm + " bpm.");
     }
 }
 
 function slow_down() {
-    if (bpm > 10) {
-        bpm = bpm - 1;
+    if (bpm > bpmMin) {
+        bpm = bpm - 5;
         transition = 60000 / bpm;
-        fade = Math.round(transition / 3);
-        console.log("slow down");
+        fade = Math.round(transition / fadeRatio);
+        console.log("Slowing down to " + bpm + " bpm.");
     }
 }
 
 function fade_on() {
-    console.log("fade on");
+    console.log("Turning fade on.");
     fadeOn = true;
 }
 
 function fade_off() {
-    console.log("fade off");
+    console.log("Turning fade off.");
     fadeOn = false;
+}
+
+function cycle() {
+    do {
+        run_pattern(currentPattern);
+    } while (cyclingOn == true);
+}
+
+function run_pattern(pattern) {
+    for (i = 0; i < 8; i++) {
+        change_color(patterns[pattern][i]);
+        sleep(transition);
+    }
 }
 
 /**
@@ -205,7 +113,6 @@ function fade_off() {
  * @param {*} transition optional transition time in milliseconds
  */
 function change_color(color, brightness = 100, transition = 0) {
-    globalColor = color;
     var opt = {};
     var colors = convert.hex.hsl(color);
     opt.hue = colors[0];
@@ -219,138 +126,7 @@ function change_color(color, brightness = 100, transition = 0) {
     }
     light.power(true, transition, opt)
         .then(status => {
-            // just commented out so
-            // I could see other output
             //console.log(status)
         })
         .catch(err => console.error(err))
-        .catch(err => console.error(err))
-        .catch(err => console.error(err))
 }
-
-function cycle_colors() {
-    do {
-        console.log("pattern mode: " + currentPattern);
-        switch (currentPattern) {
-            case 0:
-                change_color(colors0.color0, 100, fade)
-                console.log(fade);
-                sleep(transition)
-                change_color(colors0.color1, 100, fade)
-                console.log(fade);
-                sleep(transition)
-                change_color(colors0.color2, 100, fade)
-                console.log(fade);
-                sleep(transition)
-                change_color(colors0.color3, 100, fade)
-                console.log(fade);
-                sleep(transition)
-                change_color(colors0.color4, 100, fade)
-                console.log(fade);
-                sleep(transition)
-                change_color(colors0.color5, 100, fade)
-                console.log(fade);
-                sleep(transition)
-                change_color(colors0.color6, 100, fade)
-                sleep(transition)
-                change_color(colors0.color7, 100, fade)
-                sleep(transition)
-                break
-            case 1:
-                change_color(colors1.color0, 100, fade)
-                sleep(transition)
-                change_color(colors1.color1, 100, fade)
-                sleep(transition)
-                change_color(colors1.color2, 100, fade)
-                sleep(transition)
-                change_color(colors1.color3, 100, fade)
-                sleep(transition)
-                change_color(colors1.color4, 100, fade)
-                sleep(transition)
-                change_color(colors1.color5, 100, fade)
-                sleep(transition)
-                change_color(colors1.color6, 100, fade)
-                sleep(transition)
-                change_color(colors1.color7, 100, fade)
-                sleep(transition)
-                break
-            case 2:
-                change_color(colors2.color0, 100, fade)
-                sleep(transition)
-                change_color(colors2.color1, 100, fade)
-                sleep(transition)
-                change_color(colors2.color2, 100, fade)
-                sleep(transition)
-                change_color(colors2.color3, 100, fade)
-                sleep(transition)
-                change_color(colors2.color4, 100, fade)
-                sleep(transition)
-                change_color(colors2.color5, 100, fade)
-                sleep(transition)
-                change_color(colors2.color6, 100, fade)
-                sleep(transition)
-                change_color(colors2.color7, 100, fade)
-                sleep(transition)
-                break
-            case 3:
-                change_color(colors3.color0, 100, fade)
-                sleep(transition)
-                change_color(colors3.color1, 100, fade)
-                sleep(transition)
-                change_color(colors3.color2, 100, fade)
-                sleep(transition)
-                change_color(colors3.color3, 100, fade)
-                sleep(transition)
-                change_color(colors3.color4, 100, fade)
-                sleep(transition)
-                change_color(colors3.color5, 100, fade)
-                sleep(transition)
-                change_color(colors3.color6, 100, fade)
-                sleep(transition)
-                change_color(colors3.color7, 100, fade)
-                sleep(transition)
-                break
-            case 4:
-                change_color(colors4.color0, 100, fade)
-                sleep(transition)
-                change_color(colors4.color1, 100, fade)
-                sleep(transition)
-                change_color(colors4.color2, 100, fade)
-                sleep(transition)
-                change_color(colors4.color3, 100, fade)
-                sleep(transition)
-                change_color(colors4.color4, 100, fade)
-                sleep(transition)
-                change_color(colors4.color5, 100, fade)
-                sleep(transition)
-                change_color(colors4.color6, 100, fade)
-                sleep(transition)
-                change_color(colors4.color7, 100, fade)
-                sleep(transition)
-                break
-            case 5:
-                change_color(colors5.color0, 100, fade)
-                sleep(transition)
-                change_color(colors5.color1, 100, fade)
-                sleep(transition)
-                change_color(colors5.color2, 100, fade)
-                sleep(transition)
-                change_color(colors5.color3, 100, fade)
-                sleep(transition)
-                change_color(colors5.color4, 100, fade)
-                sleep(transition)
-                change_color(colors5.color5, 100, fade)
-                sleep(transition)
-                change_color(colors5.color6, 100, fade)
-                sleep(transition)
-                change_color(colors5.color7, 100, fade)
-                sleep(transition)
-                break
-        }
-    } while (cyclingOn == true);
-}
-
-function next_mode() {
-    patternMode = (patternMode + 1) % patternModeCount;
-}
-
